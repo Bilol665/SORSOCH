01304ea7cb851751914e213e59f02c5614c5ffc2
bilol665 <bilolsultan665@gmail.com>
2024-11-12 09:56:21 +0500

    Initial commit

---
+++ src/main/java/uz/bilol/sorsoch/SorsochApplication.java
@@ -0,0 +1,13 @@
+package uz.bilol.sorsoch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorsochApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorsochApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/bilol/sorsoch/SorsochApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bilol.sorsoch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorsochApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
